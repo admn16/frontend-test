54e497d3027a816bae0b3274227041fe8d1467ca
Anton Nocum <anton.nocum@gmail.com>
2018-09-10 19:58:33 +0800

    Setup webpack for React

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { hot } from 'react-hot-loader'; 
+
+const App = () => (
+  <div>
+    <h1>Hey, I'm using sasdasds!</h1>
+  </div>
+);
+
+export default hot(module)(App);
