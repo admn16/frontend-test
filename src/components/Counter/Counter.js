085a929dae880950c37a1b06bfd38d79da0a2cd2
Anton Nocum <anton.nocum@gmail.com>
2018-09-12 01:08:09 +0800

    Counter > Added style

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,6 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+const StyledCounter = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin: 5px 0;
+
+  &:hover {
+    background-color: #f7fcfd;
+  }
+`;
+
+const Title = styled.span`
+  color: #312a2a;
+  flex-grow: 10;
+  font-size: 15px;
+  max-width: 250px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Count = styled.span`
+  font-size: 14px;
+  text-align: center;
+  width: 40px;
+`;
+
+const Delete = styled.button`
+  background: none;
+  border: none;
+  color: #355869;
+  cursor: pointer;
+  font-size: 10px;
+  margin: 0 10px 0 5px;
+  outline: none;
+  
+  &:hover {
+    color: #d01204;
+  }
+`;
+
+const Increment = styled(Delete)`
+  color: #d01204;
+  font-size: 12px;
+  margin: 0;
+  
+  &:hover {
+    color: #ff1100;
+  }
+`;
+
+const Decrement = styled(Increment)`
+  color: #4791a0;
+
+  &:hover {
+    color: #4dcbe4;
+  }
+`;
 
 const Counter = ({
   count,
@@ -9,16 +67,23 @@ const Counter = ({
   onIncrement,
   onDecrement,
 }) => (
-  <div>
-    <button type="button" onClick={onDelete}>
-      [ x ]
-    </button>
-    <span>{ title }</span>
+  <StyledCounter>
+    <Delete type="button" onClick={onDelete}>
+      <i className="fas fa-trash-alt" />
+    </Delete>
 
-    <button type="button" onClick={onIncrement}>+</button>
-    <span>{ count }</span>
-    <button type="button" onClick={onDecrement}>-</button>
-  </div>
+    <Title>{ title }</Title>
+
+    <Increment type="button" onClick={onIncrement}>
+      <i className="fas fa-plus" />
+    </Increment>
+
+    <Count>{ count }</Count>
+
+    <Decrement type="button" onClick={onDecrement}>
+      <i className="fas fa-minus" />
+    </Decrement>
+  </StyledCounter>
 );
 
 Counter.propTypes = {
