8ad680973dece078169b59888c9e8d013d52d47f
Anton Nocum <anton.nocum@gmail.com>
2018-09-11 19:52:12 +0800

    Added sample styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import { hot } from 'react-hot-loader'; 
+import React, { PureComponent } from 'react';
+import { hot } from 'react-hot-loader';
+import styled from 'styled-components';
+import Card from 'components/Card/Card';
+import CounterForm from 'components/CounterForm/CounterForm';
+import * as counterApi from 'services/api';
+import 'App.css';
 
-const App = () => (
-  <div>
-    <h1>Hey, I'm using sasdasds!</h1>
-  </div>
-);
+const StyledApp = styled.main`
+  margin: 0 auto;
+  width: 400px;
+`;
+
+class App extends PureComponent {
+
+  async componentDidMount() {
+    console.log(await counterApi.getCounters());
+  }
+
+  render() {
+    return (
+      <StyledApp>
+        <CounterForm />
+
+        <article>
+          <div>
+            <button>[ x ]</button>
+            <span>Name of Counter</span>
+
+            <button>+</button>
+            <span>0</span>
+            <button>-</button>
+          </div>
+        </article>
+
+        <article>
+          <span>Total</span>
+          <span>10</span>
+        </article>
+      </StyledApp>
+    );
+  }
+}
 
 export default hot(module)(App);
---
+++ src/components/Card/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+  background: #fff;
+  border-width: 1px 1px 1px 1px;
+  border-color: rgba(90, 122, 190, 0.14);
+  border-radius: 8px;
+  border-style: solid;
+  box-shadow: 0px 3px 0px 0px rgba(90, 122, 190, 0.2);
+  padding: 10px;
+
+  &:hover {
+    border-style: solid;
+    box-shadow: 0px 10px 20px 0px rgba(90, 122, 190, 0.15);
+  }
+`;
+
+const Card = ({ children }) => (
+  <StyledCard>{ children }</StyledCard>
+);
+
+export default Card;
